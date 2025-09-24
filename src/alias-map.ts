4fdca7f4db04d75b4b697db865a3701a2a585967
chodoru <chodoru@icloud.com>
2025-09-24 14:24:40 +0300

    fixed const

--- src/alias-map.ts
+++ src/alias-map.ts
@@ -2,35 +2,48 @@ import patchedYargs from "./yargs.js";
 
 export const ALLOWED_TAGS = {
 	required: {
-		path: ["path", "p"],
-		out: ["out", "o"],
+		path: {
+			type: "",
+			alias: ["path", "p"]
+		},
+		out: {
+			type: "",
+			alias: ["out", "o"]
+		},
 	},
 	optional: {
-		name: ["name", "n"],
-		const: ["const"]
+		name: {
+			type: "",
+			alias: ["name", "n"]
+		},
+		const: {
+			type: true,
+			alias: ["const"]
+		},
 	},
 };
 
+
 export async function getTags(): Promise<
-	{ [key in keyof (typeof ALLOWED_TAGS)["required"]]: string } &{ [key in keyof (typeof ALLOWED_TAGS)["optional"]]?: string }
+	{ [key in keyof (typeof ALLOWED_TAGS.required)]: (typeof ALLOWED_TAGS.required)[key]["type"] } & { [key in keyof (typeof ALLOWED_TAGS.optional)]?: (typeof ALLOWED_TAGS.optional)[key]["type"] }
 > {
 	const patched = await Promise.resolve(patchedYargs.argv);
-	const res: { [key: string]: string } = {};
+	const res: {[key:string]:any} = {} as never;
 	Object.keys(ALLOWED_TAGS.required).forEach((key) => {
-		const aliases = ALLOWED_TAGS.required[key as keyof typeof ALLOWED_TAGS.required];
-		aliases.forEach((aliasName) => {
+		const aliasData = ALLOWED_TAGS.required[key as keyof typeof ALLOWED_TAGS.required];
+		aliasData.alias.forEach((aliasName) => {
 			if (
 				patched[aliasName] !== undefined &&
 				patched[aliasName] !== null &&
-				typeof patched[aliasName] === "string"
+				typeof patched[aliasName] === aliasData.type
 			)
 				res[key] = patched[aliasName];
 		});
 		if (!res[key]) throw new Error("MISSING TAG " + key + ". RERUN THE SCRIPT WITH IT SPECIFIED");
 	});
 	Object.keys(ALLOWED_TAGS.optional).forEach((key) => {
-		const aliases = ALLOWED_TAGS.optional[key as keyof typeof ALLOWED_TAGS.optional];
-		aliases.forEach((aliasName) => {
+		const aliasData = ALLOWED_TAGS.required[key as keyof typeof ALLOWED_TAGS.required];
+		aliasData.alias.forEach((aliasName) => {
 			if (
 				patched[aliasName] !== undefined &&
 				patched[aliasName] !== null &&
