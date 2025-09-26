fcea69c410c66dbfccfd5d864586f10700e89ed5
s5wn <s5wn@icloud.com>
2025-09-26 21:18:10 +0300

    rework type parsing

--- src/type-gen.ts
+++ src/type-gen.ts
@@ -4,19 +4,48 @@ import { promises } from "fs";
 
 type StringObject = {[key:string]: string};
 
+function getClosingBracket(s:string): [number,number] {
+  let closeIndex = -1;
+  let initial = -1;
+  let counter = 0;
+  const stack = [];
+  const whitelisted = ["}","{"];
+  const lookup: {[key:string]:string|undefined} = {"}":"{"};
+
+  for (let c of s) {
+    if (lookup[c]) {
+      if (stack.length > 0 && stack[stack.length - 1] === lookup[c]) {
+        stack.pop();
+        if (stack.length===0) closeIndex = counter;
+      } else {
+        return [-1,-1];
+      }
+    } else if (whitelisted.includes(c)) {
+      if (stack.length===0) initial = counter;
+      stack.push(c);
+    }
+    counter++;
+  }
+  return [initial,closeIndex];
+}
 export async function generateTypes(file:RecursiveObject, typeFile: string): Promise<string> {
     let toReturn = "{\n" 
     const typeData = (await promises.readFile(typeFile)).toString().trim();
     function getType(index:string) {
         const exp = new RegExp(String.raw`\s${index}\s`,"m")
         const hasMatch = typeData.match(exp);
-        if (!hasMatch) return `${index}: any \n`; ;
+        if (!hasMatch) return "";
         const firstIndex = hasMatch[0];
         const upperBound = (hasMatch.index!+firstIndex.length);
-        const substr = typeData.substring(upperBound);
-        const exp2 = /{([^}]*)}/;
-        const returnVal = substr.match(exp2)
-        return `${index}: ${returnVal?.[0] ?? "any"} \n`; 
+        const indexStart = typeData.substring(upperBound).search("{");
+        if (indexStart===-1) return "";
+        const iterStr = typeData.substring(indexStart);
+        const stack = [];
+        const [lower,upper] = getClosingBracket(iterStr);
+        if (lower>-1) {
+            return `${index}: ${typeData.substring(lower,upper)} \n`; 
+        }
+        return "";
     }
     Object.keys(file).forEach((index)=>{
         toReturn += getType(index);
